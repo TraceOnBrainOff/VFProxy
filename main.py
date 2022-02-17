3b1d53c5f39b03d6a1504bfc59f3b77a97e3e0c3
TraceOnBrainOff <sev.webmaster.k@gmail.com>
2022-02-17 05:11:05 +0100

    it's 5 AM i crave the void

---
+++ main.py
@@ -0,0 +1,12 @@
+from src.VFProxy import VFProxy
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="VFProxy", description="A VoiceForge proxy to download voice clips without logging in.")
+    parser.add_argument('--voice_name')
+    parser.add_argument('--text')
+    parser.add_argument('--encode', action='store_true')
+    args=parser.parse_args()
+    argv = vars(args)
+    proxy = VFProxy()
+    proxy.download_file(argv["voice_name"], argv["text"], argv["encode"])
---
+++ src/VFProxy.py
@@ -0,0 +1,26 @@
+import requests
+import string
+import random
+from urllib.parse import quote_plus
+import datetime
+import os
+
+class VFProxy():
+    def __init__(self):
+        mail_domain = random.choice(["@gmail.com", "@hotmail.com", "@outlook.com", "@comcast.net", "@aol.com", "@yandex.com", "@icloud.com", "@yahoo.com", "@mail.com"])
+        self.email = (''.join(random.choice(string.ascii_uppercase) for i in range(10))+mail_domain)
+        self.user_agent = 'Mozilla/5.0 (Linux; Android 7.0; SM-G930VC Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/58.0.3029.83 Mobile Safari/537.36'
+    def download_file(self, voice_name="Jerkface", text="Test", *, encode=False):
+        url = "https://api.voiceforge.com:443/swift_engine?HTTP-X-API-KEY=9a272b4&voice={0}&msg={1}&email={2}".format(voice_name, quote_plus(text), self.email)
+        today = datetime.datetime.now()
+        file_name = str("{0} - {1}".format(voice_name, today.strftime("%m-%d-%Y %H-%M-%S")))
+        
+        if os.path.isdir("./outputs")==False:
+            os.mkdir("./outputs")
+        if os.path.isdir("./outputs_mp3")==False:
+            os.mkdir("./outputs_mp3")
+        r = requests.get(url)
+        open("./outputs/{0}.wav".format(file_name), 'wb').write(r.content)
+        if encode:
+            os.system("cd pwd && lame -q0 -b128 --resample 16 ./outputs/{0} ./outputs_mp3/{1}".format(file_name+'.wav', file_name+'.mp3'))
+            return "./outputs/{0}".format(file_name+'.wav'), "./outputs_mp3/{0}".format(file_name+'.mp3')
